7eee0cf731b159af9c2f359d735681a588386820
Sooty <7614538+SootyOwl@users.noreply.github.com>
2025-01-26 15:08:21 +0000

    feat: add markdown-it-task-checkbox plugin and date formatting filter to Eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,24 @@
+const markdownItTaskCheckbox = require('markdown-it-task-checkbox');
+
 module.exports = (eleventyConfig) => {
-    // Add the interlinker plugin for obsidian wikilink support
-    eleventyConfig.addPlugin(
-      require('@photogabble/eleventy-plugin-interlinker'), {}
-    );
-    // Add an alias for the post layout
-    eleventyConfig.addLayoutAlias("post", "mylayout.njk");
-    // Set the input directory and copy the bundle.css file to the output directory
-    eleventyConfig.setInputDirectory("content");
-    eleventyConfig.addPassthroughCopy("content/bundle.css");
-  };
+  // Add the interlinker plugin for obsidian wikilink support
+  eleventyConfig.addPlugin(
+    require('@photogabble/eleventy-plugin-interlinker'), {}
+  );
+  // Add an alias for the post layout
+  eleventyConfig.addLayoutAlias("post", "mylayout.njk");
+  // Set the input directory and copy the bundle.css file to the output directory
+  eleventyConfig.setInputDirectory("content");
+  eleventyConfig.addPassthroughCopy("content/bundle.css");
+  // Add the markdown-it-task-checkbox plugin to the markdown-it parser
+  eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItTaskCheckbox));
+
+  // Add the date filter
+  eleventyConfig.addFilter("dateFormat", (date) => {
+    return new Date(date).toLocaleDateString("en-GB", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  });
+};
